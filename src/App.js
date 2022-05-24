41178cd958940ce7be96b40048b1adfaaeff46e6
Jonas Barros <jonasfelipester@gmail.com>
2022-05-23 22:51:08 -0300

    Add button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App(props) {
   return (
     <div>
     <h1>{ props.title } {props.user }</h1>
-    <input name="usuario" className="usuario" id="usuario" placeholder="Digite aqui seu usuario :::" />
+    <input name="usuario" className="usuario" id="usuario" placeholder="Digite aqui seu usuario :::" onChange={e => setUsuario(e.target.value)} />
+    <button type="button">Pesquisar</button>
     </div>
   );
 }
