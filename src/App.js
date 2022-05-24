473d553cb78eb7ab534ccc1c6bbb2aa3913da03b
Jonas Barros <jonasfelipester@gmail.com>
2022-05-24 00:13:36 -0300

    importando axios e consumindo API do Git para localização de repos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React, {useState, useEffect} from 'react';
+// importando o react e desestruturando os Hooks useState e useEffect
 import './App.css';
+import axios from 'axios';
+
 
 function App(props) {
+  const[usuario, setUsuario]=useState("");
+function handlePesquisa(){
+  axios.get(`https://api.github.com/users/${usuario}/repos`).then(response =>console.log(response));
+  console.log({usuario})
+}
   return (
-    <div>
+    <>
     <h1>{ props.title } {props.user }</h1>
-    <input name="usuario" className="usuario" id="usuario" placeholder="Digite aqui seu usuario :::" onChange={e => setUsuario(e.target.value)} />
-    <button type="button">Pesquisar</button>
-    </div>
+    <input name="usuario" className="usuario" id="usuario" placeholder="Digite aqui seu usuario::" value={usuario} onChange={e => setUsuario(e.target.value)} />
+    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
+    <p>O usuario digitado é {  usuario }</p>
+    </>
   );
 }
 
